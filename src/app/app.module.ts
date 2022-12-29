17c3d3d4b944690c5e13ce418472a2ad0cfab531
Laura Albornoz <albornoz.laurasol@gmail.com>
2022-12-29 16:15:35 -0300

    componentes generales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExpLaboralComponent } from './components/exp-laboral/exp-laboral.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +21,13 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    FooterComponent,
+    ContactoComponent,
+    ProyectosComponent,
+    SkillsComponent,
+    EducacionComponent,
+    ExpLaboralComponent
   ],
   imports: [
     BrowserModule
